6a379126415e495eb20bf2490d4d4f61be5ceaf6
SZanetti <storyzanetti@gmail.com>
2014-11-19 14:55:30 -0600

    Added functionality to update sidebar

--- js/form.js
+++ js/form.js
@@ -1,7 +1,65 @@
+var courses = [];
+var user = "coyle";
+
 $(document).on('ready', function() {
+	getCourses(function() {
+		getRubrics(function() {
+			populateClasses(function() {
+				populateForm();
+			});
+		});
+	});
+
 	$(document).on('click', '.side_bar ul li', function(event) {
 		$('.side_bar ul li').removeClass('selected');
 		$(this).addClass('selected');
 	});
 	$( "#tabs" ).tabs();
 });
+
+function getCourses(callback) {
+	$.ajax({
+        type: "GET",
+        url: "json/RosterWithOutcomes.json",
+        success: function(output) {
+            output = output.RosterWithOutcomes;
+
+            for(var i = 0; i < output.length; i++) {
+            	if(output[i].instructor === user) {
+            		courses.push(output[i]);
+            	}
+            }
+
+            callback();
+        }
+    });
+}
+
+function getRubrics(callback) {
+	$.ajax({
+        type: "GET",
+        url: "json/OutcomeDescriptionAndRubrics.json",
+        success: function(output) {
+            rubrics = output.OutcomeDescriptionAndRubrics;
+
+            callback();
+        }
+    });
+}
+
+function populateClasses(callback) {
+	var course_HTML = "";
+
+	for(var i = 0; i < courses.length; i++) {
+		if(i === 0) {
+			course_HTML += "<li class='selected'>" + courses[i].course + "</li>";
+		}
+		else {
+			course_HTML += "<li>" + courses[i].course + "</li>";
+		}
+	}
+
+	$("section#courses ul").append(course_HTML);
+
+	callback();
+}
--- js/report.js
+++ js/report.js
@@ -13,7 +13,26 @@ $(document).on('ready', function() {
 
 function populateOutcomesForSemester(semester) {
 	$.ajax({
-	  type: "GET",
-	  url: "json/CycleOfOutcomes.json"
-	});
+        type: "GET",
+        url: "json/CycleOfOutcomes.json",
+        success: function(output) {
+            var CycleOfOutcomes = output.CycleOfOutcomes;
+
+            for(var i = 0; i < CycleOfOutcomes.length; i++) {
+            	if(CycleOfOutcomes[i].semester === semester) {
+            		var CAC = CycleOfOutcomes[i].CACOutcomes;
+
+            		for(var k = 0; k < CAC.length; k++) {
+            			$('#CAC-' + CAC[k]).addClass("selected");
+            		}
+
+            		var EAC = CycleOfOutcomes[i].EACOutcomes;
+
+            		for(var k = 0; k < EAC.length; k++) {
+            			$('#EAC-' + EAC[k]).addClass("selected");
+            		}
+            	}
+            }
+        }
+    });
 }
