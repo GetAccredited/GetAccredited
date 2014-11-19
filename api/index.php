5bc6bff74406193c3b9442188332398216325a36
Jessica Yeh <jyeh@smu.edu>
2014-11-19 15:57:32 -0600

    function to validate a user by email and password

--- api/index.php
+++ api/index.php
@@ -9,20 +9,25 @@ $app = new Slim();
 $m = new MongoClient("mongodb://$dbhost");
 $db = $m->$dbname;
 
-$app->get('/getUser', 'getUser');
+$app->post('/getUser', 'getUser');
 $app->run();
 
 function getUser() {
     global $db;
 
+    // Get the email and password from the POST
+    $request = Slim::getInstance()->request();
+    $email = $request->post('email');
+    $password = $request->post('password');
+
     $users = $db->users;
 
     $user = array(
-        'email' => 'coyle@smu.edu',
-        'password' => 'asdfasdf'
+        'email' => $email,
+        'password' => $password
     );
 
     $user = $users->findOne($user);
-    var_dump($user);
+    echo json_encode($user);
 }
 ?>
