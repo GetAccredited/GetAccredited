a9da54c1b396e82c251e9fee5008092d3a42eec1
JKayse <jkayse@smu.edu>
2014-11-28 13:48:14 -0600

    Use data from the database to populate the form

--- js/report.js
+++ js/report.js
@@ -18,12 +18,12 @@ $(document).on('ready', function() {
 	});
 });
 
+//Gets the outcomes required for the semester
 function populateOutcomesForSemester(semester) {
 	$.ajax({
         type: "POST",
         url: "api/getSelectedOutcomes",
         data: {
-            //eventually replace with instructors name
             semester: semester,
         },
         success: function(output) {
@@ -38,10 +38,12 @@ function populateOutcomesForSemester(semester) {
     		for(var k = 0; k < EAC.length; k++) {
     			$('#EAC-' + EAC[k]).addClass("selected");
     		}
+            populateReport($('.side_bar ul li.selected'));
         }
     });
 }
 
+//Gets the list of outcomes
 function populateOutcomes(callback) {
     $.ajax({
         type: "GET",
@@ -60,8 +62,46 @@ function populateOutcomes(callback) {
     });
 }
 
+//Gets the report based on the selected outcomes. 
 function populateReport(outcomes) {
+    $("#report").empty();
+    var outcomeList = []
     for(var i = 0; i < outcomes.length; i++) {
-        
+        outcomeList[i] = outcomes[i].id;
     }
+    $.ajax({
+        type: "POST",
+        url: "api/generateReport",
+        data: JSON.stringify(outcomeList),
+        success: function(output) {
+            output = JSON.parse(output);
+            for(var i = 0; i < output.length; i++){
+                populateTable(output[i]);
+            }
+        }
+    });
+}
+
+//Populates the table for each selected outcome.
+function populateTable(outcome){
+    var table_html = "<article class='outcome_report'><span class='outcome_title'>Outcome ";
+    table_html += outcome.outcome + ": </span><span class='outcome_description'>";
+    table_html += outcome.description + "</span><table class='report_table'>";
+    table_html += "<tr><th>(%)</th><th>Unsatisfactory</th><th>Developing</th>";
+    table_html += "<th>Satisfactory</th><th>Exemplary</th><th>% S+E</th></tr>";
+    var results = outcome.results;
+    for(var i = 0; i < results.length; i++){
+        console.log(results[i]);
+        table_html += "<tr><th class='v_table_header'>"+ results[i].description +"</th>";
+        if(results[i].percentages !== null){
+            table_html += "<td>"+results[i].percentages[0]+"</td><td>"+results[i].percentages[1]+"</td><td>"+results[i].percentages[2]+"</td><td>"+results[i].percentages[3]+"</td><td>"+results[i].percentages[4]+"</td>";
+
+        }
+        else{
+            table_html += "<td>--</td><td>--</td><td>--</td><td>--</td><td>--</td>";
+        }
+        table_html += "</tr>";
+    }
+    table_html += "</table></article>";
+    $('#report').append(table_html);
 }
